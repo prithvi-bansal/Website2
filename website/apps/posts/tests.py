af085cdd634765ab14a803df52531c38a2d6a916
prithvi-bansal <prithvi.bansal1000@gmail.com>
2022-08-08 19:24:53 +0530

    commit 08-08-2022

--- website/apps/posts/tests.py
+++ website/apps/posts/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class DemoTestCase(TestCase):
+
+	def func(self, x):
+	    return x + 1
+
+	def test_answer(self):
+	    # assert self.func(3) == 5
+	    self.assertEqual(self.func(3), 5)
+
+	def test_one(self):
+		x = "this"
+		assert "h" in x
+
+	def test_two(self):
+		x = "hello"
+		assert hasattr(x, "check")
+
+	def test_false_is_false(self):
+		("Method: test_false_is_false.")
+		self.assertFalse(False)
+
+	def test_false_is_true(self):
+		print("Method: test_false_is_true.")
+		self.assertTrue(False)
+
+	def test_one_plus_one_equals_two(self):
+		print("Method: test_one_plus_one_equals_two.")
+		self.assertEqual(1 + 1, 2)
+
+
+class UserTestCase(TestCase):
+
+	def setUp(self):
+		user_a = User.objects.create_user(username='admin', password='1234')
+		self.user_a = user_a
+
+	def test_admin_pswd(self):
+		self.assertTrue(self.user_a.check_password('1234'))
+		
